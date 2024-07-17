1702dcf5caaa3fb4ae2ffd4497f43575832b4b0e
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-07-17 16:13:10 +0200

    create View class with constructor, getters and setters

--- app/App/View/View.php
+++ app/App/View/View.php
@@ -2,6 +2,32 @@
 
 namespace App\View;
 
-class View {
+class View
+{
+    private string $path;
 
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * Get the value of path
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set the value of path
+     *
+     * @return  View
+     */
+    public function setPath(string $path): View
+    {
+        $this->path = $path;
+
+        return $this;
+    }
 }
